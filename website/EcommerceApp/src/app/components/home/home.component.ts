b986c304267a4a527593b841ce125947ddf61529
Jishnu Suresh <jishnusuresh307@gmail.com>
2023-11-10 20:18:15 +0530

    cart works now

--- website/EcommerceApp/src/app/components/home/home.component.ts
+++ website/EcommerceApp/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api.service';
+import { CartapiService } from 'src/app/services/cartapi.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,19 @@ import { ApiService } from 'src/app/services/api.service';
 })
 export class HomeComponent {
   productList:any;
-  constructor( private api:ApiService ) {}
+  constructor( private api:ApiService,
+    private cartApi:CartapiService) {}
 
   ngOnInit(): void {
     this.api.getProduct().subscribe(res=>{
       this.productList=res;
+      this.productList.forEach((a:any)=>{
+        Object.assign(a,{quantity:1, total:a.price})
+      });
     })
+  }
+  addToCart(item:any){
+    this.cartApi.addToCart(item);
   }
 
 }
